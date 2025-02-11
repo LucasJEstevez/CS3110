28d9db2b0c6bc20760771e64917b31841df44ec5
Lucas Estevez <lucasestevez2004@gmail.com>
2025-02-11 02:40:47 -0700

    Assignment 2 - Slight Corrections
    Formatting, fixing errors, changing integer parsing slightly

--- 2 - Self Mutilating Page/jsFile.js
+++ 2 - Self Mutilating Page/jsFile.js
@@ -1,14 +1,22 @@
+//Event listener for calculator button
 const button = document.querySelector("#equalsButton");
 button.addEventListener("click", equalsClick, false);
 
+//Event listener for hovering over image
 const rick = document.querySelector("#theMan");
 rick.addEventListener("mouseover", imageListen, false);
 
+//Function to calculate
 function equalsClick(event){let text="";
 
+    //Numbers
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
-    if(Number.isInteger(parseInt(num1)) && Number.isInteger(parseInt(num2))){
+
+    number1 = parseInt(num1); number2 = parseInt(num2);
+
+    //Checks they are real numbers
+    if(Number.isInteger(number1) && Number.isInteger(number2)){
         let text='';
         let answer=0;
 
@@ -17,14 +25,14 @@ function equalsClick(event){let text="";
         switch(opt){
             case 0:
                 op="+";
-                if(num1=='6' && num2 == '9'){answer="HAHA YOURE SO FUNNY";}
-                else{answer=parseInt(num1)+parseInt(num2);}
+                if(number1==6 && number2 == 9){answer="HAHA YOURE SO FUNNY IM LITERALLY CRYING";}
+                else{answer=number1+number2;}
                 break;
-            case 1: answer=num1-num2;op="-";break;
-            case 2: answer=num1*num2;op="*";break;
-            case 3: answer=num1/num2;op="/";break;
+            case 1: answer=number1-number2;op="-";break;
+            case 2: answer=number1*number2;op="*";break;
+            case 3: answer=number1/number2;op="/";break;
         }
-        text = num1+" "+op+" "+num2+" = "+answer+"\n";
+        text = number1+" "+op+" "+number2+" = "+answer+"\n";
         console.log(text);
         
         let line = document.createElement("p");
@@ -34,7 +42,13 @@ function equalsClick(event){let text="";
         sect.appendChild(line);
     }
     else{
-        console.log("Something went wrong!");
+        let text = "Invalid! Try again."
+
+        let line = document.createElement("p");
+        line.textContent = text;
+
+        let sect = document.getElementById("answers");
+        sect.appendChild(line);
     }
 
     //This preventDefault stops the button from redirecting the page, which in this case would refresh
@@ -49,7 +63,7 @@ function imageListen(event){
     helloMsg.remove();
 
     const byeMsg = document.getElementById("byeMsg");
-    byeMsg.textContent = ":( Sad";
+    byeMsg.textContent = "Me when you let me down instead-->     :(";
 
     //Without this line, if you hover over the image but then push the button, this listener can get called again.
     //However, the original h2 tag is gone so this causes errors
