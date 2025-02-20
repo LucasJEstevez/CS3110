cdeb36e95f606927807e4d315873d701f55ad43f
Lucas Estevez <lucasestevez2004@gmail.com>
2025-02-20 15:54:48 -0700

    Catching up w/ Wesley
    This is what is getting copied over BEFORE playing with api request stuff

--- ServerFiles/deploy.sh
+++ ServerFiles/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scp html/* root@lje.crabdance.com:/var/www/jsapp
+scp html/* root@lje.crabdance.com:/var/www/html
 scp jsapp/* root@lje.crabdance.com:/var/www/jsapp
 ssh root@lje.crabdance.com systemctl restart jsapp
