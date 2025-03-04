9f072eb3e02bc6c189fd8d611c56f966a30cd62f
Lucas Estevez <lucasestevez2004@gmail.com>
2025-03-04 00:58:04 -0700

    Removed console.log stmts
    They should've been taken out last week

--- ServerFiles/jsapp/index.js
+++ ServerFiles/jsapp/index.js
@@ -8,7 +8,6 @@ const handleRequest = (req, res) => {
 
   //if going to /api
   if (req.url.includes('/api')) {
-    console.log("api url")
     const [path, query] = req.url.split('?')
 
     //if post request
@@ -21,17 +20,12 @@ const handleRequest = (req, res) => {
         const params = Object.fromEntries(body.split('&').map(
             (param) => param.split('=')
         ))
-        console.log("params",params)
         let name = params.postMeme
         let year = params.postYear
-        console.log(name,", ",year)
         validInput = (year >= 1980 && year <= 2025 && name)
-        console.log("validInput? ", validInput)
         if (validInput) {
-          console.log("valid input for post!")
           meme = {name,year}
           memes.push(meme)
-          console.log("memes,",memes)
 
           //this was the kbye function before
           res.writeHead(201, {
@@ -56,7 +50,6 @@ const handleRequest = (req, res) => {
     }
 
     else if (req.method == "GET") {
-      console.log("get method")
       let body = ''
       req.on('data', (data) => {
         body += data
@@ -64,19 +57,14 @@ const handleRequest = (req, res) => {
       req.on('end', () => {
         let params = {}
         if(query) {
-          console.log("there's a query!")
           params = Object.fromEntries(query.split('&').map(
               (param) => param.split('=')
           ))
         }
-        console.log("params",params)
         let name = params.meme
         let year = params.year
-        console.log(name,", ",year)
         validInput = (year >= 1980 && year <= 2025 && name)
-        console.log("validInput? ", validInput)
         if (validInput) {
-          console.log("valid input for get!")
 
           let matches = []
           let i = 0
@@ -88,7 +76,6 @@ const handleRequest = (req, res) => {
           }
 
           if (matches.length == 0){
-            console.log("no matches found")
             res.writeHead(404, {
               "Content-Type": "text/html"
             })
@@ -96,16 +83,12 @@ const handleRequest = (req, res) => {
             res.end()
           }
           else{
-            console.log("matches found")
-            console.log(matches)
             res.writeHead(200, {
               "Content-Type": "text/html"
             })
             res.write("<h1>Matches Found!</h1><br><h2>Memes</h2><table>")
             let index = 0;
-            console.log("entering while loop")
             while (index < matches.length) {
-              console.log("iteration ", index + 1)
               res.write("<tr><td>")
               res.write(matches[index].name)
               res.write("</td><td>")
@@ -113,7 +96,6 @@ const handleRequest = (req, res) => {
               res.write("</td>")
               index++;
             }
-            console.log("exited while loop")
             res.write("</table>")
             res.write("<br><a href='/api'>See All Memes</a>")
             res.end()
@@ -123,8 +105,7 @@ const handleRequest = (req, res) => {
         }
         else {
           //this will show all mems
-          console.log("invalid input for get")
-          res.writeHead(400, {
+          res.writeHead(200, {
             "Content-Type": "text/html"
           })
           res.write("<h1>Showing all memes!</h1>")
@@ -133,9 +114,7 @@ const handleRequest = (req, res) => {
           res.write("<h2>Memes</h2><table>")
           if(memes.length != 0){
             let index = 0;
-            console.log("entering while loop")
             while (index < memes.length) {
-              console.log("iteration ", index + 1)
               res.write("<tr><td>")
               res.write(memes[index].name)
               res.write("</td><td>")
@@ -143,13 +122,11 @@ const handleRequest = (req, res) => {
               res.write("</td>")
               index++;
             }
-            console.log("exited while loop")
             res.write("</table>")
           }
           else {res.write("<p>No memes yet!</p>")}
           res.write("<br><a href='/'>Home</a>")
           res.end()
-          console.log("exited while loop")
         }
       })
     }
