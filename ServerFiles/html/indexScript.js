9d21b397de4dfed45d00b95eb6ffa03f7fd949f5
Lucas Estevez <lucasestevez2004@gmail.com>
2025-03-05 17:30:27 -0700

    Assignment 4
    Assignment 4

---
+++ ServerFiles/html/indexScript.js
@@ -0,0 +1,4 @@
+ const sendEditReq = (num) => {
+    console.log("Your mom likes edit reqs")
+     console.log(num)
+ }
--- ServerFiles/jsapp/index.js
+++ ServerFiles/jsapp/index.js
@@ -3,6 +3,7 @@
 const http = require('http')
 
 let memes = []
+let nextID = 0;
 
 const handleRequest = (req, res) => {
 
@@ -12,6 +13,7 @@ const handleRequest = (req, res) => {
 
     //if post request
     if (req.method == "POST") {
+      console.log("POST Request")
       let body = ''
       req.on('data', (data) => {
         body += data
@@ -22,9 +24,11 @@ const handleRequest = (req, res) => {
         ))
         let name = params.postMeme
         let year = params.postYear
+        let id = nextID;
+        nextID++;
         validInput = (year >= 1980 && year <= 2025 && name)
         if (validInput) {
-          meme = {name,year}
+          meme = {name,year, id}
           memes.push(meme)
 
           //this was the kbye function before
@@ -33,7 +37,6 @@ const handleRequest = (req, res) => {
           })
           res.write("<h1>You added a meme, good job!</h1>")
           res.write("<br><a href='/'>Home</a><br><br><a href='api'>See Memes</a>")
-          //you need to figure out how to add all da memes
           res.end()
         }
         else {
@@ -50,6 +53,7 @@ const handleRequest = (req, res) => {
     }
 
     else if (req.method == "GET") {
+      console.log("GET Request")
       let body = ''
       req.on('data', (data) => {
         body += data
@@ -86,17 +90,10 @@ const handleRequest = (req, res) => {
             res.writeHead(200, {
               "Content-Type": "text/html"
             })
-            res.write("<h1>Matches Found!</h1><br><h2>Memes</h2><table>")
+            writePostAndDelete(res)
+            res.write("<h1>Matches Found!</h1><br><h2>Memes</h2>")
             let index = 0;
-            while (index < matches.length) {
-              res.write("<tr><td>")
-              res.write(matches[index].name)
-              res.write("</td><td>")
-              res.write(matches[index].year)
-              res.write("</td>")
-              index++;
-            }
-            res.write("</table>")
+            writeTable(res, matches)
             res.write("<br><a href='/api'>See All Memes</a>")
             res.end()
           }
@@ -108,27 +105,101 @@ const handleRequest = (req, res) => {
           res.writeHead(200, {
             "Content-Type": "text/html"
           })
+          writePostAndDelete(res)
           res.write("<h1>Showing all memes!</h1>")
           res.write("<p>You are on this page because you either clicked the link, or your search input was invalid</p>")
 
-          res.write("<h2>Memes</h2><table>")
+          res.write("<h2>Memes</h2>")
           if(memes.length != 0){
-            let index = 0;
-            while (index < memes.length) {
-              res.write("<tr><td>")
-              res.write(memes[index].name)
-              res.write("</td><td>")
-              res.write(memes[index].year)
-              res.write("</td>")
-              index++;
-            }
-            res.write("</table>")
+            writeTable(res,memes)
           }
           else {res.write("<p>No memes yet!</p>")}
           res.write("<br><a href='/'>Home</a>")
           res.end()
         }
       })
+    }
+
+    else if (req.method === "PUT") {
+      console.log("PUT Request")
+      let body = '';
+
+      req.on('data', (chunk) => {
+        body += chunk;
+      });
+
+      req.on('end', () => {
+        try {
+          const params = JSON.parse(body)
+          console.log(params.id)
+          let change = false
+          let i = 0
+          if(params.newYear < 1980 || params.newYear > 2025){
+            throw("invalid year")
+          }
+          while (i<memes.length && !change){
+            if(memes[i].id == params.id) {
+              memes[i].name = params.newName
+              memes[i].year = params.newYear
+              change = true
+            }
+          }
+          if (change) {
+            res.writeHead(200, {
+              "Content-Type": "application/json"
+            });
+            res.end(JSON.stringify({ success: true }));
+          } else {
+            res.writeHead(404, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ error: 'Item not found' }));
+          }
+        }
+        catch (error) {
+          console.error('Error parsing JSON:', error);
+          res.writeHead(400, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'Invalid JSON format' }));
+        }
+      });
+    }
+
+    else if (req.method === "DELETE") {
+      console.log("delete request")
+      let body = '';
+
+      req.on('data', (chunk) => {
+        body += chunk;
+      });
+
+      req.on('end', () => {
+        try {
+          const requestUrl = new URL(req.url, `http://${req.headers.host}`);
+          const id = requestUrl.searchParams.get('id')
+          console.log("id,",id)
+          let change = false
+          let i = 0
+          while (i<memes.length && !change){
+            if(memes[i].id == id) {
+              memes = memes.filter((item) => item.id !== Number(id));
+              change = true
+            }
+          }
+          if (change) {
+            res.writeHead(200, {
+              "Content-Type": "application/json"
+            });
+            res.end(JSON.stringify({ success: true }));
+          } else {
+            res.writeHead(404, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ error: 'Item not found' }));
+          }
+        }
+        catch (error) {
+          console.error('Error parsing JSON:', error);
+          res.writeHead(400, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'Invalid JSON format' }));
+        }
+        console.log(memes)
+      });
     }
 
     //if not a valid post or get request
@@ -149,15 +220,68 @@ const handleRequest = (req, res) => {
   }
 }
 
-/*
-const kbye = (res) => {
-  res.writeHead(200, {
-    "Content-Type": "application/json"
-  })
-  res.write(JSON.stringify(dancers))
-  res.end()
+//write out the search table
+const writeTable = (res, arr) => {
+  index = 0
+  res.write("<table>")
+  while (index < arr.length) {
+    res.write("<tr><td>")
+    res.write(arr[index].name)
+    res.write("</td><td>")
+    res.write(arr[index].year)
+    res.write("</td><td>")
+    res.write("<button onclick='sendEditReq(")
+    res.write(arr[index].id.toString())
+    res.write(")'>Edit</button>")
+    res.write("</td><td>")
+    res.write("<button onclick='sendDeleteReq(")
+    res.write(arr[index].id.toString())
+    res.write(")'>Delete</button>")
+    res.write("</td></tr>")
+    index++;
+  }
+  res.write("</table>")
 }
- */
+
+const writePostAndDelete = (res) => {
+
+  res.write(`
+        <script>
+            const sendEditReq = (num) => {
+                let newName = prompt("name")
+                let newYear = prompt("year")
+                console.log("sendEditReq");
+                fetch('/api', {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ id: num, newName: newName, newYear: newYear})
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                    location.reload();
+                })
+                .catch(error => console.error('Error:', error));
+            };
+            const sendDeleteReq = (num) => {
+                console.log("sendDeleteReq,", num.toString())
+                fetch('/api?id='+num.toString(), {
+                    method: 'DELETE',
+                    headers: { 'Content-Type': 'application/json' }
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                    location.reload();
+                })
+                .catch(error => console.error('Error:', error));
+            }
+        </script>
+  `)
+
+}
 
 //handleRequest is the function defined at the top of this file
 const server = http.createServer(handleRequest)
