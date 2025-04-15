2da77a7ea5f80479a8f34635c60af8e7a8159877
Lucas Estevez <lucasestevez2004@gmail.com>
2025-04-15 02:17:46 -0600

    A7

--- 7-Database_Persistence/html/indexScript.js
+++ 7-Database_Persistence/html/indexScript.js
@@ -1,3 +1,4 @@
+//GET Request to server for table data
 async function getData() {
     try {
         const response = await fetch('/api');
@@ -18,6 +19,7 @@ async function getData() {
     }
 }
 
+//Updates webpage table based on new array values (NOT a /api call)
 async function updateTable(animals, sounds) {
     console.log("updateTable")
     console.log("animals: ",animals)
@@ -34,13 +36,14 @@ async function updateTable(animals, sounds) {
         const deleteCell = row.insertCell(4);
 
         indexCell.innerText = index;
-        animalCell.innerText=animals[index]
-        soundCell.innerText=sounds[index]
+        animalCell.innerText=animals[index];
+        soundCell.innerText=sounds[index];
         editCell.innerHTML = `<button onclick="editRow(${index})">Edit</button>`;
         deleteCell.innerHTML = `<button onclick="deleteRow(${index})">Delete</button>`;
     });
 }
 
+//Adds new entry (POST)
 async function addRow() {
     let name = prompt("Name of animal:")
     let sound = prompt("Sound of animal:")
@@ -62,15 +65,47 @@ async function addRow() {
 
 }
 
+//Edits entry (PUT)
 async function editRow(index){
     console.log("edit ",index,"th row");
+
+    let name = prompt("Name of animal:")
+    let sound = prompt("Sound of animal:")
+    try {
+        const response = await fetch('/api', {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ animal:name, sound:sound, index:index }),
+        });
+    } catch (error) {
+        console.error(error.message);
+    }
+    getData().then(r => {
+        animals = r[0];
+        sounds = r[1];
+        updateTable(animals,sounds);
+    })
 }
 
+//Removes row (DELETE)
 async function deleteRow(index){
-    console.log("deleting ",index,"th row");
-    console.log(animals)
+    try {
+        const response = await fetch('/api', {
+            method: "DELETE",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ index:index }),
+        });
+    } catch (error) {
+        console.error(error.message);
+    }
+    getData().then(r => {
+        animals = r[0];
+        sounds = r[1];
+        updateTable(animals,sounds);
+    })
 }
 
+//Initial gathering of data
 getData().then(r => {
     animals = r[0];
     sounds = r[1];
---
+++ 7-Database_Persistence/jsapp/index.js
@@ -0,0 +1,213 @@
+#!/usr/bin/env node
+
+const http = require('http');
+const fs = require('fs');
+
+const ANIMALS_PATH = './animals.txt';
+const SOUNDS_PATH = './sounds.txt'
+
+const animals = []
+const sounds = []
+
+//exports data to given file
+async function pushToFile (path, arr) {
+  console.log("pushToFile has begun!")
+  console.log("path: ", path)
+  console.log("arr: ", arr)
+  try {
+    let ctnt = arr.join('\n');
+    console.log("ctnt: ", ctnt);
+    fs.writeFile(path, ctnt, (err) => {
+      if (err) {
+        console.error('Failed to write file:', err);
+      } else {
+        console.log("file go brr");
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+//gets data from given file
+async function pullFromFile (path, arr) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("data: ",data);
+      lines = data.split('\n');
+      arr.length = 0;
+      arr.push(...lines);
+      resolve();
+    })
+  })
+}
+
+//initial gathering of data form files
+console.log("about to pull from file");
+pullFromFile(ANIMALS_PATH, animals).then(() => {
+  console.log("last line");
+  console.log(animals);
+});
+pullFromFile(SOUNDS_PATH, sounds).then(() => {
+  console.log(sounds);
+});
+
+//Handles /api requests
+const handleRequest = (req, res) => {
+
+  //if going to /api
+  if (req.url.includes('/api')) {
+    console.log("api url")
+    const [path, query] = req.url.split('?')
+
+    //if post request
+    if (req.method == "POST") {
+      let body = ''
+      req.on('data', (data) => {
+        body += data
+      })
+      req.on('end', () => {
+        pullFromFile(ANIMALS_PATH, animals).then(() => {
+          pullFromFile(SOUNDS_PATH, sounds).then(() => {
+            const params = JSON.parse(body);
+            console.log("params:", params)
+            let animal = params.animal
+            let sound = params.sound
+
+            //adds new animal/sound to server data
+            animals.push(animal)
+            sounds.push(sound)
+            console.log("animals,", animals)
+            console.log("sounds,", sounds)
+
+            //adds changes to files (persistent storage)
+            console.log("pushing to files:")
+            pushToFile(ANIMALS_PATH,animals)
+            pushToFile(SOUNDS_PATH,sounds)
+
+            res.writeHead(201, {
+              "Content-Type": "text/html"
+            })
+            //you need to figure out how to add all da memes
+            res.end()
+          });
+        });
+      })
+    }
+
+    //GET request for animal data
+    else if (req.method == "GET") {
+
+      pullFromFile(ANIMALS_PATH, animals).then(() => {
+        pullFromFile(SOUNDS_PATH, sounds).then(() => {
+
+          console.log("GET")
+          res.writeHead(200, {
+            "Content-Type": "application/json"
+          })
+          const responseData = {
+            animals: animals,
+            sounds: sounds
+          };
+          res.write(JSON.stringify(responseData));
+          res.end();
+
+        });
+
+      });
+    }
+
+    //PUT request (editing rows)
+    else if (req.method == "PUT") {
+      let body = ''
+      req.on('data', (data) => {
+        body += data
+      })
+      req.on('end', () => {
+        pullFromFile(ANIMALS_PATH, animals).then(() => {
+          pullFromFile(SOUNDS_PATH, sounds).then(() => {
+            const params = JSON.parse(body);
+            console.log("params:", params)
+            let animal = params.animal;
+            let sound = params.sound;
+            let index = params.index;
+            animals[index]=animal;
+            sounds[index]=sound;
+            console.log("animals,", animals);
+            console.log("sounds,", sounds);
+
+            console.log("pushing to files:");
+            pushToFile(ANIMALS_PATH,animals);
+            pushToFile(SOUNDS_PATH,sounds);
+
+            res.writeHead(201, {
+              "Content-Type": "text/html"
+            })
+            //you need to figure out how to add all da memes
+            res.end()
+          });
+        });
+      })
+    }
+
+    //DELETE request (deleting rows)
+    else if (req.method == "DELETE") {
+      let body = ''
+      req.on('data', (data) => {
+        body += data
+      })
+      req.on('end', () => {
+        pullFromFile(ANIMALS_PATH, animals).then(() => {
+          pullFromFile(SOUNDS_PATH, sounds).then(() => {
+            const params = JSON.parse(body);
+            console.log("params:", params)
+            let index = params.index;
+            animals.splice(index, 1);
+            sounds.splice(index, 1);
+            console.log("animals,", animals);
+            console.log("sounds,", sounds);
+
+            console.log("pushing to files:");
+            pushToFile(ANIMALS_PATH,animals);
+            pushToFile(SOUNDS_PATH,sounds);
+
+            res.writeHead(201, {
+              "Content-Type": "text/html"
+            })
+
+            res.end()
+          });
+        });
+      })
+    }
+
+
+    //if not a valid request type
+    else {
+      res.writeHead(400, {
+        "Content-Type": "text/html"
+      })
+      res.write("<h1>Must Fill Out Form</h1><br><a href='/'>Home</a>")
+      res.end()
+    }
+  }
+  else{
+    res.writeHead(404, {
+      "Content-Type": "text/html"
+    })
+    res.write("<h1>Page not found, please try again later</h1><br><a href='/'>Home</a>")
+    res.end()
+  }
+}
+
+
+//handleRequest is the function defined at the top of this file
+const server = http.createServer(handleRequest)
+
+//listens on port 3000 for js requests
+server.listen(3000)
